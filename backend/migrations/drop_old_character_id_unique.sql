5c1fb2089a8b163b2f63b02d8e2fd24195134121
Kit Chestnut <kitchestnut@hotmail.com>
2025-12-26 10:49:09 -0600

    Simplify migration to drop old constraint

--- backend/migrations/drop_old_character_id_unique.sql
+++ backend/migrations/drop_old_character_id_unique.sql
@@ -1,13 +1,3 @@
--- Find and drop the old character_id unique constraint
-DO $$
-BEGIN
-    -- Try to drop the old unique constraint if it exists
-    IF EXISTS (
-        SELECT 1 FROM pg_constraint
-        WHERE conname = 'character_stats_character_id_key'
-        AND conrelid = 'character_stats'::regclass
-    ) THEN
-        ALTER TABLE character_stats DROP CONSTRAINT character_stats_character_id_key;
-        RAISE NOTICE 'Dropped constraint character_stats_character_id_key';
-    END IF;
-END $$;
+-- Drop the old character_id unique constraint
+ALTER TABLE character_stats DROP CONSTRAINT IF EXISTS character_stats_character_id_key;
+ALTER TABLE character_stats DROP CONSTRAINT IF EXISTS character_stats_character_id_unique;
